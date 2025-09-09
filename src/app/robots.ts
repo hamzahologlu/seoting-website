6039a67c123b5153a10cb612ab121673c46b4bc5
SEOTING <seoting@SEOTING-MacBook-Pro.local>
2025-09-09 11:14:19 +0300

    Japonya arama motorlarının erişimi engellendi - robots.txt güncellendi

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -23,6 +23,55 @@ export default function robots(): MetadataRoute.Robots {
           '/private/',
         ],
       },
+      // Japonya arama motorlarını engelle
+      {
+        userAgent: 'Yahoo! Slurp',
+        disallow: '/',
+      },
+      {
+        userAgent: 'YahooJP',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Y!J',
+        disallow: '/',
+      },
+      {
+        userAgent: 'goo',
+        disallow: '/',
+      },
+      {
+        userAgent: 'gooBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Baiduspider',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Baiduspider-image',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Baiduspider-video',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Baiduspider-news',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Baiduspider-favo',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Baiduspider-cpro',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Baiduspider-ads',
+        disallow: '/',
+      },
     ],
     sitemap: 'https://seoting.com/sitemap.xml',
     host: 'https://seoting.com',
